53ac0aabfc6e29fa8a174f09b6aa73a4d98ec69f
unknown <shuriken.gesellschaft@gmail.com>
2026-06-21 02:57:46 -0700

    Our Generals will be pleased!

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { SpotlightSlider_0057 } from "./sptsldqie7rh/sptsldqie7rh"
 
 
+
 function App() {
 
   return (
     <>
       <h1>Hello</h1>
       <SpotlightSlider_0057 />
+      <h3>is you there?... ring ring...</h3>
     </>
   )
 }
--- src/sptsldqie7rh/sptsldqie7rh.tsx
+++ src/sptsldqie7rh/sptsldqie7rh.tsx
@@ -2,12 +2,31 @@ import './sptsldqie7rh.css'
 import Left from './svgexport-12.svg'
 import Right from './svgexport-13.svg'
 import { useRef, useState } from 'react'
-import FerrarriBackground from './ferrarri_smallirjm3oi.webp'
-import Ferrari from './ferrari_largeoikej5dfgok.webp'
-import FerrariShoes from './ferrari_shoes12krgtp23.avif'
-import MercedesPants from './mercedes_pants94jkdk.avif'
-import MercedesTShirt from './merces_tshirt3094jkdfk.avif'
-import McLarenTShirt from './mclaren_tshirt123ikri494.avif'
+
+// First Slide
+import FerrarriBackground from '../assets/slide1/ferrarri_smallirjm3oi.webp'
+import Ferrari from '../assets/slide1/ferrari_largeoikej5dfgok.webp'
+import FerrariShoes from '../assets/slide1/ferrari_shoes12krgtp23.avif'
+import MercedesPants from '../assets/slide1/mercedes_pants94jkdk.avif'
+import MercedesTShirt from '../assets/slide1/merces_tshirt3094jkdfk.avif'
+import McLarenTShirt from '../assets/slide1/mclaren_tshirt123ikri494.avif'
+
+// Second Slide
+import GuessBackground from '../assets/slide2/guessSmallWebp23jkderj.webp'
+import Guess from '../assets/slide2/guessLarge34eipfr.avif'
+import GuessJacket from '../assets/slide2/guessJacket1weourf.avif'
+import GuessJeanJacket from '../assets/slide2/guessJeanJacket09krjkr.avif'
+import GuessTShirt from '../assets/slide2/guessTShirtWhite4ierj.avif'
+import GuessJacketWinter from '../assets/slide2/guessJacketWinter93kedje.avif'
+
+// Third Slide
+import JustCavaliCover from '../assets/slide3/justBlack398jdk.avif'
+import JustCavali from '../assets/slide3/justCavali5jikrju3.avif'
+import JustCavaliTShirt from '../assets/slide3/jCTShirt34jdjej.avif'
+import JustCavaliShoes from '../assets/slide3/jCShoe38kedj.avif'
+import JustCavaliTShirtTiger from '../assets/slide3/jCTShirtTiger094kdk.webp' 
+import JustCavaliTShirtSlogan from '../assets/slide3/jCTShirtSlogan948ikd.avif'
+
 
 interface SliderProps {
     wallpaper: string,
@@ -26,26 +45,26 @@ const first = {
     '--ws8nke7-3': '2.5'
 } as React.CSSProperties;
 
-// const second = {
-//     // '-ws8nke7-0': `linear-gradient(0deg, rgba(0, 0, 0, 0.00) 100%, rgba(0, 0, 0, 0.16) 0%), url(${Football}) rgba(0, 0, 0, 0.10)`,
-//     '-ws8nke7-0': 'black',
-//     '--ws8nke7-1': '#1a3f75;',
-//     '--ws8nke7-2': 'blur(50px)', 
-//     '--ws8nke7-3': '2.5'
-// } as React.CSSProperties;
+const second = {
+    '--ws8nke7-0': `url(${GuessBackground})`,
+    '--ws8nke7-1': 'green',
+    '--ws8nke7-2': 'blur(50px)', 
+    '--ws8nke7-3': '2.5'
+} as React.CSSProperties;
 
-// const third = {
-//     '-ws8nke7-0': `url(${Bubles})`,
-//     '--ws8nke7-1': '#24ce0d',
-//     '--ws8nke7-2': 'blur(10px)', 
-//     '--ws8nke7-3': '1.5'
-// } as React.CSSProperties;
+const third = {
+    // '--ws8nke7-0': `url(${JustCavaliCover})`,
+    // '-ws8nke7-0': `linear-gradient(0deg, rgba(0, 0, 0, 0.16) 0%, rgba(0, 0, 0, 0.00) 100%)`,
+    // '--ws8nke7-1': 'black',
+    '--ws8nke7-2': 'blur(10px)', 
+    '--ws8nke7-3': '1.5'
+} as React.CSSProperties;
 
 export const SpotlightSlider_0057 = () => {
-    const [card, setCard] = useState(2)
+    const [card, setCard] = useState(2) // this has to be 0
 
     const sliderRef = useRef<HTMLUListElement>(null)
-
+console.log(JustCavaliCover)
     const scroll = (direction: string) => {
     if (sliderRef.current) {
       const { scrollLeft, clientWidth } = sliderRef.current
@@ -67,18 +86,19 @@ export const SpotlightSlider_0057 = () => {
     scroll("left")
     setCard(card-1 >= 0 ? card-1 : card)
   }
+  console.log(card)
 
     return (
         <div className="i7jr" data-testid="spotlightSlider">
             <ul data-testid='backgroundSlider' className='yrt1j'>
-                <li>
-                    <div className='do2qk3q' style={first}></div>
+                <li data-active={card == 0 ? 'true' : 'false'}>
+                    <div className='do2qk3q irjk4gt' style={first}></div>
                 </li>
-                <li>
-                    <div className='do2qk3q' style={first}></div>
+                <li data-active={card == 1 ? 'true' : 'false'}>
+                    <div className='do2qk3q irjk4gt' style={second}></div>
                 </li>
-                <li>
-                    <div className='do2qk3q' style={first}></div>
+                <li data-active={card == 2 ? 'true' : 'false'}>
+                    <div className='do2qk3q irjk4gt jru' style={third}></div>
                 </li>
             </ul>
             <ul data-testid='sliderContent' className='sqo2j' ref={sliderRef}>
@@ -124,7 +144,7 @@ export const SpotlightSlider_0057 = () => {
                                 <span className='ifjr fh34hg'>От нашите популярни марки</span>
                                 <div className='y1tfi'>
                                     <span className='pd4j'>Разгледай Марката</span>
-                                    <span className="odj9k">F1 Fashion</span>
+                                    <span className="odj9k">GUESS JEANS</span>
                                 </div>
                                 <ul className="yhdf3i">
                                     <li className="q1kdju" data-active='true'></li>
@@ -134,21 +154,21 @@ export const SpotlightSlider_0057 = () => {
                             </div>
                             <div data-testid='contentPromo' className='thf1ik'>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={FerrariShoes} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={GuessJacket} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={MercedesPants} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={GuessJeanJacket} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={MercedesTShirt} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={GuessTShirt} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={McLarenTShirt} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={GuessJacketWinter} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                             </div>
                         </div>
                         <div data-testid='contentSlide--right' className='pro1fk'>
-                            <img src={Ferrari} alt="odfjhr" className='a2ksdjh' />
+                            <img src={Guess} alt="odfjhr" className='a2ksdjh' />
                         </div>
                     </div>
                 </li>
@@ -159,7 +179,7 @@ export const SpotlightSlider_0057 = () => {
                                 <span className='ifjr fh34hg'>От нашите популярни марки</span>
                                 <div className='y1tfi'>
                                     <span className='pd4j'>Разгледай Марката</span>
-                                    <span className="odj9k">F1 Fashion</span>
+                                    <span className="odj9k">Just Cavalli</span>
                                 </div>
                                 <ul className="yhdf3i">
                                     <li className="q1kdju" data-active='true'></li>
@@ -169,31 +189,34 @@ export const SpotlightSlider_0057 = () => {
                             </div>
                             <div data-testid='contentPromo' className='thf1ik'>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={FerrariShoes} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={JustCavaliTShirt} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={MercedesPants} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={JustCavaliShoes} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={MercedesTShirt} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={JustCavaliTShirtTiger} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                                 <div data-testid='promoCard' className='u3itjf'>
-                                    <img src={McLarenTShirt} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
+                                    <img src={JustCavaliTShirtSlogan} alt="ejoikp123fwemjiop9i" className="qoei5ke" />
                                 </div>
                             </div>
                         </div>
-                        <div data-testid='contentSlide--right' className='pro1fk'>
-                            <img src={Ferrari} alt="odfjhr" className='a2ksdjh' />
+                        <div data-testid='contentSlide--right' className='pro1fk uirjr'>
+                            <img src={JustCavaliCover} alt="odfjhr" className='a2ksdjh' />
+                            <div className='urtjh4tg'>
+                                <img src={JustCavali} alt="" className='oi5rtk' />
+                            </div>
                         </div>
                     </div>
                 </li>
             </ul>
             <div data-testid='sliderBtn' className='ut9qz'>
                 <button className='ut84f ijfer44xc' onClick={slideLeft}>
-                    <img src={Left} alt="" />
+                    <img src={Left} alt="" className='iur4jh' />
                 </button>
                 <button className='ut84f k1tj' onClick={slideRight}>
-                    <img src={Right} alt="" />
+                    <img src={Right} alt="" className='iur4jh' />
                 </button>
             </div>
         </div>
