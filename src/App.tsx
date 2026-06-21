3a758c77f193c6482427419e23f2932de38cd0fd
unknown <shuriken.gesellschaft@gmail.com>
2026-06-21 10:40:04 -0700

    push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Footer_9339 } from "./abt_ftr2134234/abt_ftr2134234"
+
+
+function App() {
+
+  return (
+    <>
+     <Footer_9339 />
+    </>
+  )
+}
+
+export default App
---
+++ src/abt_ftr2134234/staticLinks_84674/staticLinks_84674.tsx
@@ -0,0 +1,59 @@
+import './staticLinks_84674.css'
+
+export const StaticLinks_84674 = () => {
+    return (
+        <nav data-testid='links' className='oirj678kf'>
+            <section className='ptj567k'>
+                <div>
+                    <div className='kt1ij'>Категории за мъже</div>
+                    <menu className='i345rjrk'>
+                        <a href="#">Сака</a>
+                        <a href="#">Мъжки обувки Adidas Originals</a>
+                        <a href="#">Футболни обувки</a>
+                        <a href="#">Мъжки тениски</a>
+                        <a href="#">Мъжки Парки</a>
+                        <a href="#">Сникърси за мъже</a>
+                        <a href="#">Мъжко бельо Calvin Klein</a>
+                        <a href="#">Колани за мъже</a>
+                        <a href="#">Портмонета</a>
+                        <a href="#">Колани HUGO за мъже</a>
+                        <a href="#">Спортни обувки за мъже</a>
+                        <a href="#">Мъжки обувки Timberland</a>
+                        <a href="#">Часовници за мъже</a>
+                        <a href="#">Мъжки боксерки</a>
+                        <a href="#">Къси Панталони за мъже</a>
+                        <a href="#">Обувки за мъже</a>
+                        <a href="#">Ризи</a>
+                        <a href="#">Панталони за мъже</a>
+                        <a href="#">Мъжки дънки</a>
+                    </menu>
+                </div>
+                <div>
+                    <div className='kt1ij'>Категории за мъже</div>
+                    <menu className='i345rjrk'>
+                        <a href="#">Jack & Jones</a>
+                        <a href="#">Under Armour</a>
+                        <a href="#">DIESEL</a>
+                        <a href="#">JOOP!</a>
+                        <a href="#">POLO RALPH LAUREN</a>
+                        <a href="#">G-Star RAW</a>
+                        <a href="#">ALPHA INDUSTRIES</a>
+                        <a href="#">DRYKORN</a>
+                        <a href="#">LACOSTE</a>
+                        <a href="#">SCOTCH & SODA</a>
+                        <a href="#">Adidas Originals</a>
+                        <a href="#">BOSS Casual</a>
+                        <a href="#">HOLLISTER</a>
+                        <a href="#">OLYMP</a>
+                        <a href="#">Superdry</a>
+                        <a href="#">TOMMY HILFIGER</a>
+                        <a href="#">BURTON MENSWEAR LONDON</a>
+                        <a href="#">HUGO</a>
+                        <a href="#">Petrol Industries</a>
+                        <a href="#">Tommy Jeans</a>
+                    </menu>
+                </div>
+            </section>
+        </nav>
+    )
+}
